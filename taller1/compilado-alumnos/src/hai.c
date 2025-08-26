5488197c8f61f738cf7d311b79d16b18d1594f1d
ariel8sche <arielschenone@Desktop-Linux-Ariel.168.1.7>
2025-08-26 11:49:26 -0300

    ejercicio 3

--- taller1/compilado-alumnos/src/hai.c
+++ taller1/compilado-alumnos/src/hai.c
@@ -1,13 +1,46 @@
-#include <signal.h> /* constantes como SIGINT*/
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/reg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
+#include /* constantes como SIGINT*/
+#include #include #include #include #include #include #include
+int intentos = 5;
+
+void ya_va_hand() {
+	pid_t my_parent = getppid();
+
+	printf("Quedan %d intentos\n", intentos);
+	intentos--;
+	printf("Ya va!\n");
+
+	if (!intentos)
+		kill(my_parent, SIGINT);
+
+}
+
+void interrupt_hand() {
+	pid_t child = wait(NULL);
+}
 
 int main(int argc, char* argv[]) {
-  	// Completar
+	char* bin_path = argv[1];
+
+	int bin_args_q = argc - 1;
+
+	pid_t my_child = fork();
+
+	if (!my_child) {
+		signal(SIGURG, ya_va_hand);
+		printf("Soy hijo\n");
+		sleep(10);
+	} else {
+		signal(SIGINT, interrupt_hand);
+		sleep(1);
+		printf("Soy padre\n");
+		for (int i = 0; i < 5; i++) {
+			sleep(1);
+			printf("sup!\n");
+			kill(my_child, SIGURG);
+		}
+		//exit(EXIT_SUCCESS);
+	}
+
+	execvp(bin_path, argv++);
 	return 0;
 }

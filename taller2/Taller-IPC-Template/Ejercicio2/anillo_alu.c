c45e685721856ddfafa023e58a297baad40a8ea0
franco2335 <francoperotti52@gmail.com>
2025-09-12 20:14:52 -0300

    Update anillo_alu.c

--- taller2/Taller-IPC-Template/Ejercicio2/anillo_alu.c
+++ taller2/Taller-IPC-Template/Ejercicio2/anillo_alu.c
@@ -1,61 +1,89 @@
-#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <time.h>
-#include "constants.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-int generate_random_number(){
-	return (rand() % 50);
+#define READ 0
+#define WRITE 1
+
+int main(int argc, char *argv[]) {
+
+    int n = atoi(argv[1]); // cant hijos
+    int c = atoi(argv[2]); // valor 
+    int s = atoi(argv[3]); // hijo inicial
+
+    int pipes[n][2];
+    for (int i = 0; i < n; i++) {
+        pipe(pipes[i]);
+    }
+
+    int padre_pipe[2];
+    pipe(padre_pipe);
+
+    pid_t pid[n];
+    for (int i = 0; i < n; i++) {
+        pid[i] = fork();
+        if (pid[i] == 0 && i != s) {      
+            for (int j = 0; j < n; j++) {
+                if (j != i) close(pipes[j][WRITE]);
+                if (j != (i-1+n)%n) close(pipes[j][READ]);
+            }
+            close(padre_pipe[READ]);
+            close(padre_pipe[WRITE]);
+            int num;
+            while (1) {
+                read(pipes[(i-1+n)%n][READ], &num, sizeof(int));
+                num++;
+                write(pipes[i][WRITE], &num, sizeof(int));
+            }
+        }
+
+        if (i == s && pid[i] == 0) { 
+            int num;
+            for (int j = 0; j < n; j++) {
+                if (j != i) close(pipes[j][WRITE]);
+                if (j != (i-1+n)%n) close(pipes[j][READ]);
+            }
+
+            int secreto = c + 1 + rand() % 10; 
+            printf("Hijo %d: secreto= %d\n", i, secreto);
+            fflush(stdout); 
+            read(pipes[s][READ], &num, sizeof(int));
+
+            while (1) {
+                if (num >= secreto) {
+                    write(padre_pipe[WRITE], &num, sizeof(int));
+                    sleep(5); 
+                }
+                num++;
+                write(pipes[i][WRITE], &num, sizeof(int));
+                read(pipes[(i-1+n)%n][READ], &num, sizeof(int));
+            }
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        close(pipes[i][READ]);
+        if (i != s){ 
+            close(pipes[i][WRITE]);
+        }
+        }
+    close(padre_pipe[WRITE]);
+    write(pipes[s][WRITE], &c, sizeof(int)); // el padre tiene q escribir en el pipe del hijo, si escribi en el suyo y que el hijo lea de ahi el read de abajo no se traba 
+
+    int resultado;
+    read(padre_pipe[READ], &resultado, sizeof(int)); // este
+    printf("Resultado final: %d\n", resultado);
+
+    for (int i = 0; i < n; i++) {
+        if (pid[i] > 0) kill(pid[i], SIGKILL);
+    }
+
+    for (int i = 0; i < n; i++) wait(NULL);
+
+    return 0;
 }
-
-int main(int argc, char **argv)
-{	
-	//Funcion para cargar nueva semilla para el numero aleatorio
-	srand(time(NULL));
-
-	int status, pid, n, start, buffer;
-	n = atoi(argv[1]);
-	buffer = atoi(argv[2]);
-	start = atoi(argv[3]);
-
-	n = 3;
-
-	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
-    
-  	/* COMPLETAR */
-    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer, start);
-    
-    /* COMPLETAR */
-	pid_t *children = malloc(sizeof(*children) * n);
-    
-	int **pipe_list = malloc(sizeof(int*) * (n));
-
-	for (int i = 0; i < n - 1; i++) {
-		pipe_list[i] = malloc(sizeof(int) * 2);
-
-		pipe(pipe_list[i]);
-
-		printf("File descs: %d y %d\n", pipe_list[i][0], pipe_list[i][1]);
-	}
-
-	for (int i = 0; i < n; i++) {
-		pid_t pid = fork();
-        if (i == start, pid == 0){
-			
-        
-		}
-		if (pid == 0) {
-			while (1) {
-				int my_number;
-				read(pipe_list[i][PIPE_READ], &my_number, sizeof(my_number));
-				my_number++;
-				write(pipe_list[i+1][PIPE_READ], &my_number,sizeof(my_number));
-			}
-		} else {
-			children[i] = pid;
-		}
-	}
-}
-
